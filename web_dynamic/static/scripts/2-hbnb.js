e7d87184a0709a2c49879bfe919ca3ffc0128985
root <root@pc>
2023-04-24 19:01:51 +0200

    get the api status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
 $('document').ready(function () {
+  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
+  $.get(url, function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
   const listAmenities = {};
   $('input[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
@@ -8,13 +17,5 @@ $('document').ready(function () {
       delete listAmenities[$(this).attr('data-id')];
     }
     $('.amenities H4').text(Object.values(listAmenities).join(', '));
-  });
-
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
   });
 });
